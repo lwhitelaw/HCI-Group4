faa59a478adf7428b066cb01fe84888b0e01d16b
lwhitelaw <48133276+lwhitelaw@users.noreply.github.com>
2023-03-09 18:06:35 -0500

    Bare minimum setup

---
+++ src/main/java/hci4/ui/Hci4UiApplication.java
@@ -0,0 +1,12 @@
+package hci4.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hci4UiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Hci4UiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hci4/ui/Hci4UiApplicationTests.java
@@ -0,0 +1,13 @@
+package hci4.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hci4UiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
