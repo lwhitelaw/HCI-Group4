a91401895afa37d9a3c791968783b9bfaa7a3281
Zachary Hower <zeek1496@gmail.com>
2023-04-04 21:34:35 -0400

    AddWorkClockWiidget

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -115,3 +115,32 @@ function currentDate(){
 	
 	document.getElementById("todayDate").innerText = today;
 }
+
+function currentTimeWidget() {
+  let date = new Date(); 
+  let hh = date.getHours() + timeZoneValue;
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+  let session = "AM";
+  var timeZoneValue = document.getElementById("timeZone").value;
+
+  if(hh === 0) {
+      hh = 12;
+  }
+  if(hh > 12){
+      hh = hh - 12;
+      session = "PM";
+   }
+
+   hh = (hh < 10) ? "0" + hh : hh;
+   mm = (mm < 10) ? "0" + mm : mm;
+   ss = (ss < 10) ? "0" + ss : ss;
+    
+   let time = hh + ":" + mm + ":" + ss + " " + session;
+
+   document.getElementById("digitalClockWidget").innerText = time; 
+   let t = setTimeout(function(){ currentTimeWidget() }, 1000);
+}
+
+
+
